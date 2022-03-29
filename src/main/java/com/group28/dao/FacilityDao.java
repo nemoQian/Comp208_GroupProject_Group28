c7bea71cc2a3bcbd6365d2d2e5aa961e068faf8a
Yucheng Du <sgydu9@liverpool.ac.uk>
2022-03-29 20:05:59 +0100

    新任务的方法接口

---
+++ src/main/java/com/group28/dao/FacilityDao.java
@@ -0,0 +1,49 @@
+package com.group28.dao;
+
+import com.group28.pojo.ElectricityUnit;
+import com.group28.pojo.ElectricityUnitType;
+import com.group28.pojo.ElectricityUnitData;
+
+/**
+ * The interface Electricity Facility.
+ *
+ * @author Yucheng Du
+ * @version 1.0
+ */
+
+public interface FacilityDao {
+
+
+    /**
+     * Gets user.
+     *
+     * @param FacilityId, ZipCode, FacilityType, Name
+     * @return Added Facility
+     */
+    ElectricityUnit AddFacility(String FacilityId, String ZipCode, String FacilityType, String Name);
+
+    /**
+     * Gets user.
+     *
+     * @param TypeID, MAxConsumption, MinConsumption
+     * @return Specific Facility
+     */
+    ElectricityUnitType AddType(String TypeID, int MaxConsumption, int MinConsumption);
+
+    /**
+     * Gets user.
+     *
+     * @param FacilityId
+     * @return Specific Facility
+     */
+    ElectricityUnit SearchFacility(String FacilityId);
+
+
+    /**
+     * Gets user.
+     *
+     * @param ReferenceID, FacilityID, TimeOfStore, Consumption
+     * @return Specific Facility
+     */
+    ElectricityUnitData InsertData(String ReferenceID, String FacilityID, String TimeOfStore, int Consumption);
+}
