48c38cb1cd59fb11d2ce4500a463186f24aedee8
suoyi <13389279870@163.com>
2022-03-31 21:57:14 +0100

    interlayer

--- home page/chart_js/chart_3.js
+++ home page/chart_js/chart_3.js
@@ -9,6 +9,7 @@ $(function () {
         var myChart = echarts.init(document.getElementById('chart_3'));
         myChart.setOption({
             tooltip: {
+                trigger: 'item',
             },
             title: {x: 222},
             color: ['blue', 'green'],
@@ -58,7 +59,7 @@ $(function () {
             encode: {
                 tooltip: 2
             },
-            series: {
+            series: [{
                 type: 'effectScatter', symbolSize: 20,
                 color: '#5e1e1e',
                 itemStyle: {
@@ -70,38 +71,29 @@ $(function () {
                 encode: {
                     tooltip: 2
                 },
-                data: [
-                    [770.3415644319939, 757.9672194986475, 30],
-                    [1180.0329284196291, 743.6141808346214, 80],
-                    [894.03790632245, 1188.1985153835008, 61],
-                    [1072.98925630313, 477.3839988649537, 70],
-                    [1078.62251255796, 935.6708486282843, 81]],
-            },
+                data: [],
+            }],
         });
 
 
         /*    myChart.showLoading();*/    //数据加载完之前先显示一段简单的loading动画
-/*        var names=[];    //类别数组（实际用来盛放X轴坐标值）
-        var nums=[];    //销量数组（实际用来盛放Y坐标值）*/
+        var names=[];    //类别数组（实际用来盛放X轴坐标值）
+        var nums=[];    //销量数组（实际用来盛放Y坐标值）
 
-/*        function dssx(){
+        function dss(){
             $.ajax({
                 type : "post",
                 async : true,            //异步请求（同步请求将会锁住浏览器，用户其他操作必须等待请求完成才可以执行）
-                url : "chart_js/best.json",    //请求发送到TestServlet处
+                url : "chart_js/chart_3.json",    //请求发送到TestServlet处
                 dataType : "json",        //返回数据形式为json
                 success : function(result) {
                     //请求成功时执行该函数内容，result即为服务器返回的json对象
                     if (result) {
                         $.each(result.list, function (index, item) {
-                            nums.push(item.kW);    //挨个取出类别并填入类别数组
-                            names.push(item.part);
+                            nums.push(item);    //挨个取出类别并填入类别数组
                         });
-                        /!*myChart.hideLoading(); *!/   //隐藏加载动画
+                        /*myChart.hideLoading(); */   //隐藏加载动画
                         myChart.setOption({        //加载数据图表
-                            yAxis: {
-                                data: names
-                            },
                             series: [
                                 {
                                     data: nums
@@ -121,9 +113,9 @@ $(function () {
                     myChart.hideLoading();
                 }
             })
-            setTimeout(dssx,500);
+            setTimeout(dss,500);
         }
-        dssx();*/
+        dss();
 
     }
 
