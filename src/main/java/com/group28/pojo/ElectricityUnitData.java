02fb0dd1595a92ced4fc9c370fd7aa27228bb0f3
nemoQian <nemo.qian@outlook.com>
2022-04-04 21:00:47 +0100

    new data

--- src/main/java/com/group28/pojo/ElectricityUnitData.java
+++ src/main/java/com/group28/pojo/ElectricityUnitData.java
@@ -8,97 +8,50 @@ package com.group28.pojo;
  */
 public class ElectricityUnitData {
 
-    private final String DATA_REFERENCE_ID;
-    private ElectricityUnit electricityUnit;
+    private String DATA_REFERENCE_ID;
+    private String electricityUnitId;
     private String timeOfStore;
     private int ConsumptionAmount;
-
-    /**
-     * Instantiates a new Electricity unit data.
-     *
-     * @param DATA_REFERENCE_ID the data reference id
-     * @param electricityUnit   the electricity unit
-     * @param timeOfStore       the time of store
-     * @param consumptionAmount the consumption amount
-     */
-    public ElectricityUnitData(String DATA_REFERENCE_ID, ElectricityUnit electricityUnit,
-                               String timeOfStore, int consumptionAmount) {
-        this.DATA_REFERENCE_ID = DATA_REFERENCE_ID;
-        this.electricityUnit = electricityUnit;
-        this.timeOfStore = timeOfStore;
-        ConsumptionAmount = consumptionAmount;
-    }
-
-    /**
-     * Gets data reference id.
-     *
-     * @return the data reference id
-     */
-    public String getDATA_REFERENCE_ID() {
-        return DATA_REFERENCE_ID;
-    }
-
-    /**
-     * Gets electricity unit.
-     *
-     * @return the electricity unit
-     */
-    public ElectricityUnit getElectricityUnit() {
-        return electricityUnit;
-    }
-
-    /**
-     * Sets electricity unit.
-     *
-     * @param electricityUnit the electricity unit
-     */
-    public void setElectricityUnit(ElectricityUnit electricityUnit) {
-        this.electricityUnit = electricityUnit;
-    }
-
-    /**
-     * Gets time of store.
-     *
-     * @return the time of store
-     */
-    public String getTimeOfStore() {
-        return timeOfStore;
-    }
-
-    /**
-     * Sets time of store.
-     *
-     * @param timeOfStore the time of store
-     */
-    public void setTimeOfStore(String timeOfStore) {
-        this.timeOfStore = timeOfStore;
-    }
-
-    /**
-     * Gets consumption amount.
-     *
-     * @return the consumption amount
-     */
-    public int getConsumptionAmount() {
-        return ConsumptionAmount;
-    }
-
-    /**
-     * Sets consumption amount.
-     *
-     * @param consumptionAmount the consumption amount
-     */
-    public void setConsumptionAmount(int consumptionAmount) {
-        ConsumptionAmount = consumptionAmount;
-    }
 
     @Override
     public String toString() {
         return "ElectricityUnitData{" +
                 "DATA_REFERENCE_ID='" + DATA_REFERENCE_ID + '\'' +
-                ", electricityUnit=" + electricityUnit +
+                ", electricityUnitId='" + electricityUnitId + '\'' +
                 ", timeOfStore='" + timeOfStore + '\'' +
                 ", ConsumptionAmount=" + ConsumptionAmount +
                 '}';
+    }
+
+    public String getDATA_REFERENCE_ID() {
+        return DATA_REFERENCE_ID;
+    }
+
+    public void setDATA_REFERENCE_ID(String DATA_REFERENCE_ID) {
+        this.DATA_REFERENCE_ID = DATA_REFERENCE_ID;
+    }
+
+    public String getElectricityUnitId() {
+        return electricityUnitId;
+    }
+
+    public void setElectricityUnitId(String electricityUnitId) {
+        this.electricityUnitId = electricityUnitId;
+    }
+
+    public String getTimeOfStore() {
+        return timeOfStore;
+    }
+
+    public void setTimeOfStore(String timeOfStore) {
+        this.timeOfStore = timeOfStore;
+    }
+
+    public int getConsumptionAmount() {
+        return ConsumptionAmount;
+    }
+
+    public void setConsumptionAmount(int consumptionAmount) {
+        ConsumptionAmount = consumptionAmount;
     }
 }
