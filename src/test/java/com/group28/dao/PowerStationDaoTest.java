5afd39d88edda999d43320d77e8124ea4cb10b28
Yucheng Du <sgydu9@liverpool.ac.uk>
2022-04-07 12:48:15 +0100

    更新了新增powerstation type的测试，测试成功

---
+++ src/test/java/com/group28/dao/PowerStationDaoTest.java
@@ -0,0 +1,45 @@
+package com.group28.dao;
+
+import com.group28.pojo.PowerStation;
+import com.group28.pojo.PowerStationType;
+import com.group28.util.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+public class PowerStationDaoTest {
+    @Test
+    public void insertTypeTest(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        PowerStationDao mapper = sqlSession.getMapper(PowerStationDao.class);
+
+
+        PowerStationType NewType = new PowerStationType();
+        NewType.Put_type_Id("NP");
+        NewType.Put_MAX_PRODUCTION(1500);
+
+        mapper.AddStationType(NewType);
+
+        sqlSession.commit();
+        sqlSession.close();
+        System.out.println("Finish");
+
+    }
+
+    @Test
+    public void insertStationTest(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        PowerStationDao mapper = sqlSession.getMapper(PowerStationDao.class);
+
+        PowerStation NewStation = new PowerStation();
+        NewStation.Put_powerStationId("NU1S1Z01");
+        NewStation.Put_zipCode("S1 Z01");
+        NewStation.Put_powerStationType("NU");
+        NewStation.Put_powerStationName("Chernobyl Station");
+
+        mapper.AddStation(NewStation);
+        sqlSession.commit();
+        sqlSession.close();
+
+        System.out.println("Finish");
+    }
+}
